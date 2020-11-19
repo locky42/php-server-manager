f9cc30681772c9c5bad02b09dcc16776906c2913
Max <podzh42@gmail.com>
2020-11-19 18:35:46 +0200

    v0.1

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp ./example.com.conf /etc/apache2/sites-available/example.com.conf
+cp ./addhost /usr/bin/addhost
+chmod +x /usr/bin/addhost
+cp ./server /usr/bin/server
+chmod +x /usr/bin/server
